5408148c67dd4dea8749a8995de732a0a3c060de
NithinVJames <njames1607@gmail.com>
2023-11-28 21:18:47 +0530

    chore: html,css completed

---
+++ main.js
@@ -0,0 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz3PNuRcDbEzlKKjfV3kYGCBqeBddyCelwOXYqPIW_oOWwo2Fzr-Kj4YmxQD0n6XLEUNQ/exec'
+const form = document.forms['prayer-request']
+
+form.addEventListener('submit', e => {
+    var name1 = form['name'].value;
+    var re1 = form['request'].value;
+    fetch(`https://api.telegram.org/bot5270203551:AAHmDszAG_3LpScU-EO6xamxV1kBuBN-tqs/sendMessage?chat_id=-737082532&Text=name%20:%20${name1}%0ARequest%20:%20${re1}`);
+    
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(() => {
+        $("#form_alerts").html("<div class='alert alert-success'>Requested successfully.</div>");
+        setTimeout(() => {$("#form_alerts").html('')},2000)
+        document.getElementById("pr").reset();
+    })
+    .catch(() => $("#form_alerts").html("<div class='alert alert-danger'>Error.</div>"))
+})
